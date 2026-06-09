76f44d3360814329e5a0a3f8e6fdce3977769855
Sharon Moegi <sharamoegi@gmail.com>
2026-06-10 00:19:19 +0300

    Create class

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,53 @@
 class Song:
-    pass
+    # Class attributes to track global library insights
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artists_count = {}
+
+    def __init__(self, name, artist, genre):
+        # Instance attributes
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Trigger class methods automatically upon instantiation
+        Song.add_song_to_count()
+        Song.add_to_genres(self.genre)
+        Song.add_to_artists(self.artist)
+        Song.add_to_genre_count(self.genre)
+        Song.add_to_artists_count(self.artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        """Increments the total value of song count by one."""
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        """Adds unique genres to the global genres list."""
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        """Adds unique artists to the global artists list."""
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        """Tracks the exact frequency distribution of each music genre."""
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artists_count(cls, artist):
+        """Tracks the exact frequency distribution of songs per artist."""
+        if artist in cls.artists_count:
+            cls.artists_count[artist] += 1
+        else:
+            cls.artists_count[artist] = 1
